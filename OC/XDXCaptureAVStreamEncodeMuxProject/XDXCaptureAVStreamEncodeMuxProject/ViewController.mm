71da1fbb420cbfe324c0514adc87e9658763ff5f
demonli <demonli@tvunetworks.com>
2019-07-07 11:57:49 +0800

    Add 3rd: ffmpeg, single file, log4cplus

---
+++ OC/XDXCaptureAVStreamEncodeMuxProject/XDXCaptureAVStreamEncodeMuxProject/ViewController.mm
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XDXCaptureAVStreamEncodeMuxProject
+//
+//  Created by 小东邪 on 2019/7/7.
+//  Copyright © 2019 小东邪. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
