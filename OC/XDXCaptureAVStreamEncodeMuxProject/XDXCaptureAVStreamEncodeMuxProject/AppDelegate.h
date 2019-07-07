83e8a02568a744fb97c75fcffba9b8bc79277b03
demonli <demonli@tvunetworks.com>
2019-07-07 11:49:37 +0800

    Init Project

---
+++ OC/XDXCaptureAVStreamEncodeMuxProject/XDXCaptureAVStreamEncodeMuxProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XDXCaptureAVStreamEncodeMuxProject
+//
+//  Created by 小东邪 on 2019/7/7.
+//  Copyright © 2019 小东邪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OC/XDXCaptureAVStreamEncodeMuxProject/XDXCaptureAVStreamEncodeMuxProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XDXCaptureAVStreamEncodeMuxProject
+//
+//  Created by 小东邪 on 2019/7/7.
+//  Copyright © 2019 小东邪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
